1571981df9cd96d8ba5cef643d963636cebc8e6e
Jacob Krall <jacob@fogcreek.com>
2013-07-18 20:44:47 -0400

    4. "Pretty print" timestamps

--- kata.js
+++ kata.js
@@ -32,11 +32,45 @@ function linkify(str) {
     });
 }
 
+const SECONDS = 1;
+const MINUTES = 60*SECONDS;
+const HOURS = 60*MINUTES;
+const DAYS = 24*HOURS;
+const WEEKS = 7*DAYS;
+const MONTHS = 30*DAYS;
+const YEARS = 365*DAYS;
+
+function formatDate(str_date) {
+   var time = new Date(str_date).getTime();
+   var now = new Date().getTime();
+   var diff = (now - time) / 1000;
+
+   var result;
+
+   if(diff < 1*MINUTES) {
+       result = "Less than a minute ago";
+   } else if(diff < 1*HOURS) {
+       result = "About " + Math.floor(diff / MINUTES) + " minutes ago";
+   } else if(diff < 1*DAYS) {
+       result = "About " + Math.floor(diff / HOURS) + " hours ago";
+   } else if(diff < 1*WEEKS) {
+       result = "About " + Math.floor(diff / DAYS) + " days ago";
+   } else if(diff < 1*MONTHS) {
+       result = "About " + Math.floor(diff / WEEKS) + " weeks ago";
+   } else if(diff < 1*YEARS) {
+       result = "About " + Math.floor(diff / MONTHS) + " months ago";
+   } else {
+       result = "More than " + Math.floor(diff / YEARS) + " years ago";
+   }
+
+   return "<time datetime='" + str_date + "'>" + result + "</time>\n";
+}
+
 function formatTweet(tweet) {
     return "\n<DIV CLASS='tweet'>\n" +
     "<H1>" + linkify(tweet.userName) + "</H1>" +
     "<P>" + linkify(tweet.message) + "</P>" +
-    "<time>" + tweet.date + "</time>" +
+    formatDate(tweet.date) +
     "\n</DIV>\n";
 }
 
